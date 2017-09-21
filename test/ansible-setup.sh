0ac9ddeafbc232930454e52dc51d60e65a0ae385
Anton Ustyuzhanin <au@hellofresh.com>
2017-09-21 16:57:28 +0200

    Fix AVM env vars

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -7,17 +7,17 @@ SETUP_VERSION="v1.0.0"
 #SETUP_VERBOSITY="vv"
 
 ## Install Ansible 2.0
-ANSIBLE_VERSIONS[0]="2.0.2.0"
-INSTALL_TYPE[0]="pip"
-ANSIBLE_LABEL[0]="v2.0"
+ANSIBLE_VERSIONS_0="2.0.2.0"
+INSTALL_TYPE_0="pip"
+ANSIBLE_LABEL_0="v2.0"
 
 ## Install Ansible 2.2
-ANSIBLE_VERSIONS[1]="2.2.0.0"
-INSTALL_TYPE[1]="pip"
-ANSIBLE_LABEL[1]="v2.2"
+ANSIBLE_VERSIONS_1="2.2.0.0"
+INSTALL_TYPE_1="pip"
+ANSIBLE_LABEL_1="v2.2"
 
 # Whats the default version
-ANSIBLE_DEFAULT_VERSION="v2.0"
+ANSIBLE_DEFAULT_VERSION="v2.2"
 
 ## Create a temp dir
 filename=$( echo ${0} | sed 's|/||g' )
