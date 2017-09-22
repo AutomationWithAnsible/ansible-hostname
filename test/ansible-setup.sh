6abf835486979ec7d0617004e5b2486f25eb1ade
Anton Ustyuzhanin <antonu17@gmail.com>
2017-09-22 11:54:10 +0200

    Fix hostname changing (#3)
    * Fix hostname changing
    
    * Update AVM
    
    * Fix Ubuntu Xenial container
    
    * Fix AVM env vars
    
    * Update ansible-setup.sh script
    
    * Remove .kitchen directory
    
    * Rollback tasks
    
    * Add .gitignore
    
    * Add python to xenial
    
    * Move hostname task after /etc/hosts modification
    
    * Remove old tests
    
    * Rename tests
    
    * Disable retry files creation

--- test/ansible-setup.sh
+++ test/ansible-setup.sh
@@ -3,21 +3,25 @@ set -e
 echo "Running travis "
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-SETUP_VERSION="v0.0.5"
-#SETUP_VERBOSITY="vv"
+SETUP_VERSION="v1.0.0"
+export AVM_VERBOSE="vv"
 
-## Install Ansible 2.0
-ANSIBLE_VERSIONS[0]="2.0.2.0"
-INSTALL_TYPE[0]="pip"
-ANSIBLE_LABEL[0]="v2.0"
+# 21 September 2017 (@antonu17)
+# pip is not work in xenial w/o LC_ALL 
+export LC_ALL="C"
 
-## Install Ansible 2.2
-ANSIBLE_VERSIONS[1]="2.2.0.0"
-INSTALL_TYPE[1]="pip"
-ANSIBLE_LABEL[1]="v2.2"
+## Install Ansible 2.2.3.0 using pip and label it 'v2.2'
+export ANSIBLE_VERSIONS_1="2.2.3.0"
+export INSTALL_TYPE_1="pip"
+export ANSIBLE_LABEL_1="v2.2"
+
+## Install Ansible 2.3.1.0 using pip and label it 'v2.3'
+export ANSIBLE_VERSIONS_2="2.3.1.0"
+export INSTALL_TYPE_2="pip"
+export ANSIBLE_LABEL_2="v2.3"
 
 # Whats the default version
-ANSIBLE_DEFAULT_VERSION="v2.0"
+export ANSIBLE_DEFAULT_VERSION="v2.3"
 
 ## Create a temp dir
 filename=$( echo ${0} | sed 's|/||g' )
